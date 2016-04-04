aa3fe7d679c9842d9da0eb311f2cce163071fb94
Andrew Fink <andrewfink8@gmail.com>
2016-04-04 11:27:46 -0700

    grabbing picture and repo number and displaying

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
+import Profile from './github/Profile.jsx';
 
 class App extends Component{
     constructor(props){
@@ -11,11 +12,32 @@ class App extends Component{
             perPage: 5
         }
     }
+
+  // get user data from github
+  getUserData(){
+      $.ajax({
+          url: 'https://api.github.com/users/'+this.state.username+'?client_id='+this.props.clientId+'&client_secret='+this.props.clientSecret,
+          dataType: 'json',
+          cache: false,
+          success: function(data){
+              this.setState({userData: data});
+              console.log(data);
+          }.bind(this),
+          error: function(xhr, status, err){
+              this.setState({username: null});
+              alert(err);
+          }.bind(this)
+      });
+  }
+
+  componentDidMount(){
+      this.getUserData();
+  }
+
   render(){
       return(
         <div>
-        hello world that has access to the internet
-        {this.state.username}
+            <Profile userData = {this.state.userData} />
         </div>
       )
   }
