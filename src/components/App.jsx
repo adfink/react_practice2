f3c15a004478b4865d2264f3853729d9355a238a
Andrew Fink <andrewfink8@gmail.com>
2016-04-04 10:17:30 -0700

    building out specific foundation for this github replica app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,13 +2,32 @@ import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            username: 'adfink',
+            userData: [],
+            userRepos: [],
+            perPage: 5
+        }
+    }
   render(){
       return(
         <div>
         hello world that has access to the internet
+        {this.state.username}
         </div>
       )
   }
+}
+
+App.propTypes = {
+    clientId: React.PropTypes.string,
+    clientSecret: React.PropTypes.string
+};
+App.defaultProps ={
+    clientId: '179b6f12211dfe10ca63',
+    clientSecret: '651cc0deb31244b22897220ecd18704753524f06'
 }
 
 export default App
