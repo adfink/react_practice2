987bf1e9d05848651b6675b31ddf0398e2130d55
Andrew Fink <andrewfink8@gmail.com>
2016-04-04 14:03:57 -0700

    bringing in da repos

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -21,7 +21,6 @@ class App extends Component{
           cache: false,
           success: function(data){
               this.setState({userData: data});
-              console.log(data);
           }.bind(this),
           error: function(xhr, status, err){
               this.setState({username: null});
@@ -30,8 +29,30 @@ class App extends Component{
       });
   }
 
+
+
+    // get user repos from github
+    getUserRepos(){
+        $.ajax({
+            url: 'https://api.github.com/users/'+this.state.username+'/repos?per_page'+this.state.perPage+'&client_id='+this.props.clientId+'&client_secret='+this.props.clientSecret+'&sort=created',
+            dataType: 'json',
+            cache: false,
+            success: function(data){
+                this.setState({userRepos: data});
+            }.bind(this),
+            error: function(xhr, status, err){
+                this.setState({username: null});
+                alert(err);
+            }.bind(this)
+        });
+    }
+
+
+
+
   componentDidMount(){
       this.getUserData();
+      this.getUserRepos();
   }
 
   render(){
---
+++ src/components/github/RepoList.jsx
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+
+class RepoList extends Component{
+
+  render(){
+      return(
+      <div className="panel panel-default">
+        
+      </div>
+      )
+  }
+}
+
+
+export default RepoList
